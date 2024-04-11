7312b1c68b771b9b2b3e7e16e99c444ff40f0c89
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2024-04-11 18:27:13 +1000

    remove unrequired comment in header

--- blankslate/header.php
+++ blankslate/header.php
@@ -10,15 +10,8 @@
 <div id="wrapper" class="hfeed">
 <header id="header" role="banner">
 <div id="branding">
-<!-- <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-<!-- <?php
-if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; }
-echo '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" rel="home" itemprop="url"><span itemprop="name">' . esc_html( get_bloginfo( 'name' ) ) . '</span></a>';
-if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; }
-?> -->
-</div> -->
-<div id="site-description"<?php if ( !is_single() ) { echo ' itemprop="description"'; } ?>><?php bloginfo( 'description' ); ?></div>
-</div>
+
+
 <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
 <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
 <div id="search"><?php get_search_form(); ?></div>
