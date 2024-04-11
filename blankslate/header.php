61906f44a4f5d007f02a9059ccecadab6928e6ac
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2024-04-11 17:56:27 +1000

    remove top left link

--- blankslate/header.php
+++ blankslate/header.php
@@ -10,13 +10,13 @@
 <div id="wrapper" class="hfeed">
 <header id="header" role="banner">
 <div id="branding">
-<div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-<?php
+<!-- <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+<!-- <?php
 if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; }
 echo '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" rel="home" itemprop="url"><span itemprop="name">' . esc_html( get_bloginfo( 'name' ) ) . '</span></a>';
 if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; }
-?>
-</div>
+?> -->
+</div> -->
 <div id="site-description"<?php if ( !is_single() ) { echo ' itemprop="description"'; } ?>><?php bloginfo( 'description' ); ?></div>
 </div>
 <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
